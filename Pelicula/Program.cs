d03d5add2c0c1bbbc6bd89c728f3bbcd8966f2aa
FutureOfOrlando <60374663+FutureOfOrlando@users.noreply.github.com>
2020-02-21 21:09:51 -0800

    Actividad 1 Versión Final

--- Pelicula/Program.cs
+++ Pelicula/Program.cs
@@ -5,38 +5,33 @@ namespace Pelicula
     class Program
     {
         public class Peliculas{
-            public string Titulo;
-            public string getTitulo()=> Titulo;
-            public void setTitulo(string T)=> Titulo = T; 
-            /*-----------------------------------------------------------*/
-            public int Año;
-            public int getAño()=> Año;
-            public void setAño(int A)=> Año =A;
-            /*-----------------------------------------------------------*/
-            public string Pais;
-            public string getPais()=> Pais;
-            public void setPais(string P)=>  Pais = P;
-            /*-----------------------------------------------------------*/
-            public string Director;
-            public string getDirector()=> Director;
-            public void setDirector(string D)=> Director = D;
+            private string Titulo;
+            private int Año;
+            private string Pais;
+            private string Director;
+                public Peliculas(){
+            }
+                public Peliculas(string Titulo,int Año){
+                    this.Titulo=Titulo;
+                    this.Año=Año;
+                }
+                public void Imprime(){
+                    Console.WriteLine("Película: {0} Año: {1}.", Titulo, Año);
+                }
         }
-
+        
 
 
         static void Main(string[] args)
         {
-            Peliculas P1 = new Peliculas();
-            Peliculas P2 = new Peliculas();
+            Peliculas P1 = new Peliculas("Titanic", 1997);
             /*-----------------------------------------------------------*/
-            P1.setTitulo("Titanic");
-            P1.setAño(1997);
-            Console.WriteLine("{0}({1})", P1.getTitulo(), P1.getAño());
+            Peliculas P2 = new Peliculas("Ben-Hur", 2003);
             /*-----------------------------------------------------------*/
-            P2.setTitulo("Ben-Hur");
-            P2.setAño(2003);
-            Console.WriteLine("{0}({1})", P2.getTitulo(), P2.getAño());
+            P1.Imprime();
             /*-----------------------------------------------------------*/
+            P2.Imprime();
+
         }
     }
 }
