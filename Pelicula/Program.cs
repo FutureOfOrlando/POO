c59dc9a6bd94aaefc0c52286df80914859ef3a11
FutureOfOrlando <60374663+FutureOfOrlando@users.noreply.github.com>
2020-02-21 18:53:33 -0800

    Dos peliculas ganadoras a Oscar

--- Pelicula/Program.cs
+++ Pelicula/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace Pelicula
 {
     class Program
@@ -9,14 +9,14 @@ namespace Pelicula
             public string getTitulo()=> Titulo;
             public void setTitulo(string T)=> Titulo = T; 
             public int Año;
-            public string getAño()=> Año;
-            public void setAño(string A)=> Año =A;
+            public int getAño()=> Año;
+            public void setAño(int A)=> Año =A;
             public string Pais;
             public string getPais()=> Pais;
-            public void setPais(int P)=>  Pais = P;
+            public void setPais(string P)=>  Pais = P;
             public string Director;
             public string getDirector()=> Director;
-            public void set(string D)=> Director = D;
+            public void setDirector(string D)=> Director = D;
         }
 
 
@@ -26,10 +26,13 @@ namespace Pelicula
             Peliculas P1 = new Peliculas();
             Peliculas P2 = new Peliculas();
             
-            P1.setTitulo("Pelicula 1");
-            P1.setAño(2020);
-            Console.WriteLine("{0}({1})", P1.GetTitulo(), P1.GetAño());
-
+            P1.setTitulo("Titanic");
+            P1.setAño(1997);
+            Console.WriteLine("{0}({1})", P1.getTitulo(), P1.getAño());
+            P2.setTitulo("Ben-Hur");
+            P2.setAño(2003);
+            Console.WriteLine("{0}({1})", P2.getTitulo(), P2.getAño());
+            
         }
     }
 }
