b384cee6459d2913248b31b9d39a2e973b14ac1d
FutureOfOrlando <60374663+FutureOfOrlando@users.noreply.github.com>
2020-05-19 21:00:37 -0700

    a

---
+++ Cola/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+namespace Cola 
+{
+    public class Cola<T>
+    {
+    readonly int m_Size;
+    int m_StackPointer = 0;
+    T[] m_Items;
+    public Cola():this(3)
+    {}
+    public Cola(int size)
+    {
+        m_Size = size;
+        m_Items = new T[m_Size];
+    }
+    public void Push(T item)
+    {
+        if(m_StackPointer>= m_Size)
+            throw new StackOverflowException();
+        
+            m_Items[m_StackPointer] = item;
+            m_StackPointer++;
+    }
+    public T Pop(){
+        this.m_StackPointer--;
+        if(m_StackPointer>= 0){
+            var C = m_Items[0];
+            for(int i = 0;i<m_StackPointer;i++)
+                m_Items[i] = m_Items[i+1];
+            return C;
+            
+        }
+        else
+            m_StackPointer = 0;
+            throw new InvalidOperationException("Cannot pop an empty stack");
+    }
+}
+    class Program{
+         static void Main(string[] args){
+            var L = new Cola<int>();
+            for(int i = 0; i<3; i++){
+                L.Push(i+1);}
+            for(int i = 0; i<3; i++)
+                Console.Write(L.Pop());
+                Console.WriteLine();
+                var N = new Char[]{'A','B','C'};
+                var text = new Cola<char>(N.Length);
+            for(int i = 0; i<N.Length; i++)
+                text.Push(N[i]);
+            for(int i = 0; i<N.Length; i++)
+                Console.Write(text.Pop());
+         }
+    }
+}
--- Stack/Program.cs
+++ Stack/Program.cs
@@ -1,26 +1,18 @@
 ﻿using System;
-
 namespace Stack
 {
     public class Stack<T>
-
     {
-
    readonly int m_Size; 
-
    int m_StackPointer = 0;
-
-   public T[] m_Items;//Quitar public
-
+   public T[] m_Items;
    public Stack():this(10)
    {}
    public Stack(int size)
-
    {
       m_Size = size;
       m_Items = new T[m_Size];
    }
-
    public void Push(T item)
    {
       if(m_StackPointer >= m_Size) 
@@ -35,47 +27,29 @@ namespace Stack
       if(m_StackPointer >= 0)
       {
          return m_Items[m_StackPointer];
-
       }
-
       else
-
       {
-
          m_StackPointer = 0;
-
          throw new InvalidOperationException("Cannot pop an empty stack");
-
       }
-
    }
-
 }
-
     class Program
     {
         static void Main(string[] args)
         {
            Stack<int> Pila = new Stack<int>();
-
            Pila.Push(15);
            Pila.Push(12);
            Pila.Push(8);
            Pila.Push(17);
-
            foreach(int item in Pila.m_Items)
                 Console.WriteLine(item);
                 Console.WriteLine(Pila.Pop());
                 Console.WriteLine(Pila.Pop());
-           
            foreach(int item in Pila.m_Items)
                 Console.WriteLine(item);
-
-           
-
-
-
-
         }
     }
 }
