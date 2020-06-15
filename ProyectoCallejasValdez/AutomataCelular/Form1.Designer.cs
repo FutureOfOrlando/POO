44e2d4de55c639eda6427d5f0ecb7af8c0f38f5f
FutureOfOrlando <60374663+FutureOfOrlando@users.noreply.github.com>
2020-06-15 12:06:55 -0700

    Update

---
+++ ProyectoCallejasValdez/AutomataCelular/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace AutomataCelular
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Ramdom = new System.Windows.Forms.Button();
+            this.Pausar = new System.Windows.Forms.Button();
+            this.Iniciar = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.Generacion = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Limpiar = new System.Windows.Forms.Button();
+            this.Save1 = new System.Windows.Forms.Button();
+            this.Save2 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(17, 46);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(400, 400);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // Ramdom
+            // 
+            this.Ramdom.Font = new System.Drawing.Font("Ink Free", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Ramdom.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Ramdom.Location = new System.Drawing.Point(598, 333);
+            this.Ramdom.Margin = new System.Windows.Forms.Padding(4);
+            this.Ramdom.Name = "Ramdom";
+            this.Ramdom.Size = new System.Drawing.Size(168, 40);
+            this.Ramdom.TabIndex = 0;
+            this.Ramdom.Text = "Random";
+            this.Ramdom.UseVisualStyleBackColor = true;
+            this.Ramdom.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Pausar
+            // 
+            this.Pausar.Enabled = false;
+            this.Pausar.Font = new System.Drawing.Font("Ink Free", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Pausar.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Pausar.Location = new System.Drawing.Point(598, 370);
+            this.Pausar.Margin = new System.Windows.Forms.Padding(4);
+            this.Pausar.Name = "Pausar";
+            this.Pausar.Size = new System.Drawing.Size(168, 40);
+            this.Pausar.TabIndex = 1;
+            this.Pausar.Text = "Pausar";
+            this.Pausar.UseVisualStyleBackColor = true;
+            this.Pausar.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Iniciar
+            // 
+            this.Iniciar.Font = new System.Drawing.Font("Ink Free", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Iniciar.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Iniciar.Location = new System.Drawing.Point(598, 406);
+            this.Iniciar.Margin = new System.Windows.Forms.Padding(4);
+            this.Iniciar.Name = "Iniciar";
+            this.Iniciar.Size = new System.Drawing.Size(168, 40);
+            this.Iniciar.TabIndex = 2;
+            this.Iniciar.Text = "Iniciar";
+            this.Iniciar.UseVisualStyleBackColor = true;
+            this.Iniciar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Ink Free", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 30);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Generación:";
+            // 
+            // Generacion
+            // 
+            this.Generacion.AutoSize = true;
+            this.Generacion.Font = new System.Drawing.Font("Ink Free", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Generacion.Location = new System.Drawing.Point(131, 14);
+            this.Generacion.Name = "Generacion";
+            this.Generacion.Size = new System.Drawing.Size(25, 30);
+            this.Generacion.TabIndex = 8;
+            this.Generacion.Text = "1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Ink Free", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(294, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 30);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Célula: ";
+            // 
+            // Limpiar
+            // 
+            this.Limpiar.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Limpiar.Location = new System.Drawing.Point(432, 431);
+            this.Limpiar.Name = "Limpiar";
+            this.Limpiar.Size = new System.Drawing.Size(75, 23);
+            this.Limpiar.TabIndex = 3;
+            this.Limpiar.Text = "Limpiar";
+            this.Limpiar.UseVisualStyleBackColor = true;
+            this.Limpiar.Click += new System.EventHandler(this.Limpiar_Click);
+            // 
+            // Save1
+            // 
+            this.Save1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Save1.Location = new System.Drawing.Point(9, 460);
+            this.Save1.Name = "Save1";
+            this.Save1.Size = new System.Drawing.Size(80, 23);
+            this.Save1.TabIndex = 4;
+            this.Save1.Text = "Partida 1";
+            this.Save1.UseVisualStyleBackColor = true;
+            this.Save1.Click += new System.EventHandler(this.Save1_Click);
+            // 
+            // Save2
+            // 
+            this.Save2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Save2.Location = new System.Drawing.Point(95, 460);
+            this.Save2.Name = "Save2";
+            this.Save2.Size = new System.Drawing.Size(80, 23);
+            this.Save2.TabIndex = 5;
+            this.Save2.Text = "Partida 2";
+            this.Save2.UseVisualStyleBackColor = true;
+            this.Save2.Click += new System.EventHandler(this.Save2_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox2.Location = new System.Drawing.Point(10, 40);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(414, 414);
+            this.pictureBox2.TabIndex = 10;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(779, 488);
+            this.Controls.Add(this.Save2);
+            this.Controls.Add(this.Save1);
+            this.Controls.Add(this.Limpiar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Generacion);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Ramdom);
+            this.Controls.Add(this.Pausar);
+            this.Controls.Add(this.Iniciar);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Font = new System.Drawing.Font("Ink Free", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Game of life";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Main_MouseClick);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button Ramdom;
+        private System.Windows.Forms.Button Pausar;
+        private System.Windows.Forms.Button Iniciar;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Generacion;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Limpiar;
+        private System.Windows.Forms.Button Save1;
+        private System.Windows.Forms.Button Save2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ ProyectoCallejasValdez/AutomataCelular/Form1.cs
@@ -0,0 +1,369 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace AutomataCelular
+{
+    public partial class Main : Form
+    {
+        //Variables respectivas de las dimensiones
+        private int Longitud = 40;
+        private int LongitudPixel = 10;
+        int[,] celulas = new int[40,40]; 
+
+        public Main()
+        {
+            InitializeComponent();
+
+            //Matriz por defecto
+            celulas = new int[Longitud, Longitud];
+            try
+            {
+                List<Save> saves = new List<Save>();
+                saves = SaveBD.ReadFromTXT(@".\MatrizPorDefecto.txt");
+                int f = 0, l = 0;
+                foreach (Save p in saves)
+                {
+                    celulas[l, f] = p.celulasBd;
+                    f++;
+                    if (f == 40)
+                    {
+                        l++;
+                        f = 0;
+                    }
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                MessageBox.Show("No se encontro el archivo: MatrizPorDefecto.txt", "Error");
+                Close();
+            }
+            
+            PintarMatriz();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            PintarMatriz();
+        }
+        /*-------------------------------Fin del código para matriz por defecto-------------------------*/
+        private void PintarMatriz()
+        {   /*Método que pinta la matriz en el bitmap*/
+            Bitmap bmp = new Bitmap(pictureBox1.Width,pictureBox1.Height);
+            for (int x=0;x<Longitud;x++)
+            {
+                for (int y=0;y<Longitud;y++)
+                {
+                    if (celulas[x, y] == 0)
+                        PintarPixel(bmp, x, y, Color.Black);
+                    else
+                        PintarPixel(bmp, x, y, Color.White);
+                }
+            }
+            pictureBox1.Image = bmp;
+        }
+        private void PintarPixel(Bitmap bmp, int x, int y,Color color)
+        {
+            for ( int i=0;i<LongitudPixel; i++)
+                for(int j=0;j<LongitudPixel; j++)
+                bmp.SetPixel(i+ (x*LongitudPixel),j+(y*LongitudPixel),color);
+            
+        }
+        /*-----------------------------------Fin del código para pintar-------------------------------------*/
+        
+        private void JuegoDeLaVida()
+        {   /*Código del juego tanto como el funcionamiento como las reglas*/
+            int[,] celulasTemp = new int[Longitud, Longitud];
+            for (int x = 0; x < Longitud; x++)
+            {
+                for (int y = 0; y < Longitud; y++)
+                {
+                    if (celulas[x, y] == 0)
+                        celulasTemp[x, y] = ReglaJuegoVida(x,y,false);
+                    else
+                        celulasTemp[x, y] = ReglaJuegoVida(x, y, true);
+                }
+            }
+
+            celulas = celulasTemp;
+
+        }
+
+        private int ReglaJuegoVida(int x, int y, bool EsViva)
+        {
+            int VecinasVivas = 0;
+            /*1 2 3
+              4 # 5
+              6 7 8*/
+            //vecina 1
+            if (x > 0&&y>0)
+            if (celulas[x - 1, y - 1] == 1)
+                VecinasVivas++;
+            //vecina 2
+            if (y > 0)
+                if (celulas[x, y - 1] == 1)
+                    VecinasVivas++;
+            //vecina 3
+            if (x < Longitud - 1 && y > 0)
+                if (celulas[x + 1, y - 1] == 1)
+                    VecinasVivas++;
+            //vecina 4
+            if (x > 0)
+                if (celulas[x - 1, y] == 1)
+                    VecinasVivas++;
+            //vecina 5
+            if (x < Longitud - 1)
+                if (celulas[x + 1, y] == 1)
+                    VecinasVivas++;
+            //vecina 6
+            if (x > 0 && y < Longitud - 1)
+                if (celulas[x - 1, y + 1] == 1)
+                    VecinasVivas++;
+            //vecina 7
+            if (y < Longitud - 1)
+                if (celulas[x, y + 1] == 1)
+                    VecinasVivas++;
+            //vecina 8
+            if (x < Longitud - 1 && y < Longitud - 1)
+                if (celulas[x + 1, y + 1] == 1)
+                    VecinasVivas++;
+            if (EsViva)
+                return (VecinasVivas == 2 || VecinasVivas == 3) ? 1 : 0;
+            else
+                return VecinasVivas == 3 ? 1 : 0;
+
+        }
+        /*-------------------------------Fin de las reglas y el juego en sí----------------------------------*/
+        private void button3_Click(object sender, EventArgs e)
+        {
+            /*Botón de matriz Random*/
+            for (int i = 0; i < Longitud; i++)
+                for (int j = 0; j < Longitud; j++)
+                    celulas[i, j] = 0;
+            Random r = new Random();
+            for (int i = 0; i < Longitud; i++)
+                for (int j = 0; j < Longitud; j++)
+                    celulas[i, j] = r.Next(0,2);
+
+            PintarMatriz();
+            Generacion.Text = "1";
+        }
+        /*----------------------------------------*/
+        private void button1_Click(object sender, EventArgs e)
+        {   /*Botón de inicio*/
+            timer1.Enabled = true;
+            Pausar.Enabled = true;
+            Iniciar.Enabled = false;
+            
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {   /*Botón de pausa*/
+            timer1.Enabled = false;
+            Pausar.Enabled = false;
+            Iniciar.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            JuegoDeLaVida();
+            PintarMatriz();
+            //Generación 
+            if (Generacion.Text == "?")
+            {
+                int G = 0;
+                G++;
+                Generacion.Text = G.ToString();
+            }
+            else 
+            {
+                int G = int.Parse(Generacion.Text);
+                G++;
+                Generacion.Text = G.ToString();
+            }
+            //Guardado de las partidas en tiempo real
+            List<Save> saves2 = new List<Save>();
+            if (Save1.Enabled == false)
+            {
+                for (int l = 0; l < 40; l++)
+                {
+                    for (int f = 0; f < 40; f++)
+                    {
+                        saves2.Add(new Save(celulas[l, f]));
+                    }
+                }
+                SaveBD.WriteToTXT("MatrizSave1.txt", saves2);
+            }
+            else if (Save2.Enabled == false)
+            {
+                for (int l = 0; l < 40; l++)
+                {
+                    for (int f = 0; f < 40; f++)
+                    {
+                        saves2.Add(new Save(celulas[l, f]));
+                    }
+                }
+                SaveBD.WriteToTXT("MatrizSave2.txt", saves2);
+            }
+            else { }
+            
+            /*-----------------------Fin del guardado------------------------*/
+        }
+
+        private void Main_MouseClick(object sender, MouseEventArgs e)
+        {
+            
+            
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {   /*Código que indica la celula clickeada*/
+            this.label2.Text = "Célula: " + String.Format("{0}, {1}", e.X/10 + 1, e.Y/10 + 1);
+            if (celulas[e.X / 10, e.Y / 10] == 1)
+                celulas[e.X / 10, e.Y / 10] = 0;
+            else if (celulas[e.X / 10, e.Y / 10] == 0)
+                celulas[e.X / 10, e.Y / 10] = 1;
+
+            PintarMatriz();
+        }
+        /*------------------------------------------------------------------------------------------------*/
+        /*parte de código que guarda las matrices y las lee*/
+
+        class Save
+        {
+            public Save(int celulasBd)
+            {
+                this.celulasBd = celulasBd;
+            }
+            public int celulasBd { get; set; }
+        }
+
+        class SaveBD
+        {
+
+            public static void WriteToTXT(string path, List<Save> saves2)
+            {
+                
+                StreamWriter txtOut = new StreamWriter(
+                                      new FileStream(path, FileMode.OpenOrCreate, FileAccess.Write));
+                foreach (Save p in saves2)
+                {
+                    txtOut.Write("{0},", p.celulasBd);
+                }
+                txtOut.Close();
+            }
+
+            public static List<Save> ReadFromTXT(string path)
+            {
+                List<Save> saves = new List<Save>();
+                StreamReader txtIn = new StreamReader(
+                                     new FileStream(path, FileMode.Open, FileAccess.Read));
+                while (txtIn.Peek() != -1)
+                {
+                    string Line = txtIn.ReadLine();
+                    string[] columns = Line.Split(',');
+                    
+                    for (int l = 0; l < 1600; l++)
+                    {
+                        Save p = new Save(int.Parse(columns[l]));
+                        saves.Add(p);
+                    }
+                }
+                txtIn.Close();
+                return saves;
+            }
+        }
+        /*-------------------------------------Fin de guardar y leer---------------------------------------*/
+        private void Limpiar_Click(object sender, EventArgs e)
+        {   /*Limpiar el picture*/
+            for (int x = 0; x < Longitud; x++)
+            {
+                for (int y = 0; y < Longitud; y++)
+                {
+                    celulas[x, y] = 0;
+                }
+                
+            }
+            PintarMatriz();
+            Generacion.Text = "0";
+            timer1.Enabled = false;
+            Iniciar.Enabled = true;
+            Pausar.Enabled = false;
+        }
+
+        private void Save1_Click(object sender, EventArgs e)
+        {   /*Cambio al puerto de guardado uno y carga de la partida del slot 1*/
+            Save1.Enabled = false;
+            Save2.Enabled = true;
+            Generacion.Text = "?";
+            //Matriz Slot 1
+            celulas = new int[Longitud, Longitud];
+            List<Save> saves2 = new List<Save>();
+            try
+            {
+                saves2 = SaveBD.ReadFromTXT(@".\MatrizSave1.txt");
+            }
+            catch (FileNotFoundException)
+            {
+                MessageBox.Show("No se encontro el archivo: MatrizPorSave2.txt", "Error"); 
+                Close();
+            }
+                int f = 0, l = 0;
+            foreach (Save p in saves2)
+            {
+                celulas[l, f] = p.celulasBd;
+                f++;
+                if (f == 40)
+                {
+                    l++;
+                    f = 0;
+                }
+                
+            }
+            
+            PintarMatriz();
+
+        }
+
+        private void Save2_Click(object sender, EventArgs e)
+        {   /*Cambio al puerto de guardado dos y carga de la partida del slot 2*/
+            Save2.Enabled = false;
+            Save1.Enabled = true;
+            Generacion.Text = "?";
+            //Matriz Slot 2
+            celulas = new int[Longitud, Longitud];
+            List<Save> saves2 = new List<Save>();
+            try
+            {
+                saves2 = SaveBD.ReadFromTXT(@".\MatrizSave2.txt");
+            }
+            catch (FileNotFoundException)
+            {
+                MessageBox.Show("No se encontro el archivo: MatrizSave2.txt", "Error"); 
+                Close();
+            }
+            int f = 0, l = 0;
+            foreach (Save p in saves2)
+            {
+                celulas[l, f] = p.celulasBd;
+                f++;
+                if (f == 40)
+                {
+                    l++;
+                    f = 0;
+                }
+            }
+            PintarMatriz();
+
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
